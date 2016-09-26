deb42c70b649622e53cdab2a5ee7d2f699796334
Mitch LeBlanc <supermitch@gmail.com>
2016-09-25 20:17:12 -0700

    Add Luti game to sandbox - initial commit

---
+++ luti/conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+	t.title = "Scrolling Shooter Tutorial"
+	t.version = "0.10.1"
+	t.window.width = 480
+	t.window.height = 800
+
+	t.console = true  -- For Windows debugging
+end
---
+++ luti/main.lua
@@ -0,0 +1,13 @@
+debug = true
+
+function love.load(arg)
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw(dt)
+    love.graphics.print("hello world", 100, 100)
+end
